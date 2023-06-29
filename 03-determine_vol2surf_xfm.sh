842e357bbd872f7f114486cfe8ede3a3628a0fd1
marcjaskir <marc.jaskir@pennmedicine.upenn.edu>
2023-06-29 15:40:41 -0400

    Added antsRegistration call

--- 03-determine_vol2surf_xfm.sh
+++ 03-determine_vol2surf_xfm.sh
@@ -44,23 +44,43 @@ for sub_dir in ${SUBJECTS_DIR}/sub-*; do
                 ${output_dir}/${sub}/nu.mgz \
                 ${output_dir}/${sub}/nu.nii.gz
 
-    # Compute transformation from Freesurfer-derived nu.mgz image to QSIPrep preprocessed T1w image using flirt
-    flirt -in ${output_dir}/${sub}/nu.nii.gz \
-          -ref ${output_dir}/${sub}/${sub}_desc-preproc_T1w.nii.gz \
-          -out ${output_dir}/${sub}/nu_in_desc-preproc_T1w.nii.gz \
-          -omat ${output_dir}/${sub}/nu_in_desc-preproc_T1w.mat
-    
-    # Convert FSL to nifti 'world' affine (flirt)
+    # Compute affine for warping Freesurfer-derived nu.mgz image to QSIPrep preprocessed T1w image
+    # flirt -in ${output_dir}/${sub}/nu.nii.gz \
+    #       -ref ${output_dir}/${sub}/${sub}_desc-preproc_T1w.nii.gz \
+    #       -out ${output_dir}/${sub}/nu_in_desc-preproc_T1w.nii.gz \
+    #       -omat ${output_dir}/${sub}/nu_in_desc-preproc_T1w.mat
+
+    antsRegistrationSyNQuick.sh -d 3 -f ${output_dir}/${sub}/nu.nii.gz -m ${output_dir}/${sub}/${sub}_desc-preproc_T1w.nii.gz -n 4 -o ${output_dir}/${sub}/nu_in_desc-preproc_T1w_
+
+    antsRegistration \
+    --collapse-output-transforms 1 \
+    --dimensionality 3 \
+    --initial-moving-transform [ ${output_dir}/${sub}/${sub}_desc-preproc_T1w.nii.gz, ${output_dir}/${sub}/nu.nii.gz, 0 ] \
+    --initialize-transforms-per-stage 0 \
+    --interpolation HammingWindowedSinc \
+    --output [ ${output_dir}/${sub}/nu_in_desc-preproc_T1w_, ${output_dir}/${sub}/nu_in_desc-preproc_T1w_Warped.nii.gz ] \
+    --transform Rigid[ 0.2 ] \
+    --metric Mattes[ ${output_dir}/${sub}/${sub}_desc-preproc_T1w.nii.gz, ${output_dir}/${sub}/nu.nii.gz, 1, 32, Random, 0.25 ] \
+    --convergence [ 10000x1000x10000x10000, 1e-06, 10 ] \
+    --smoothing-sigmas 7.0x3.0x1.0x0.0vox \
+    --shrink-factors 8x4x2x1 \
+    --write-composite-transform 0
+
+    # Convert affine
+    ConvertTransformFile 3 ${output_dir}/${sub}/nu_in_desc-preproc_T1w_0GenericAffine.mat ${output_dir}/${sub}/nu_in_desc-preproc_T1w_0GenericAffine.txt
+
+    # Edit affine header to be acceptable for wb_command -convert-affine
+    sed -i '' -e 's|Transform: AffineTransform_double_3_3|Transform: MatrixOffsetTransformBase_double_3_3|' ${output_dir}/${sub}/nu_in_desc-preproc_T1w_0GenericAffine.txt
+
+    # Convert affine to nifti 'world' affine
     wb_command -convert-affine \
-                -from-flirt ${output_dir}/${sub}/nu_in_desc-preproc_T1w.mat \
-                ${output_dir}/${sub}/nu.nii.gz \
-                ${output_dir}/${sub}/${sub}_desc-preproc_T1w.nii.gz \
-                -to-world ${output_dir}/${sub}/nu_in_desc-preproc_T1w.affine
+                -from-itk ${output_dir}/${sub}/nu_in_desc-preproc_T1w_0GenericAffine.txt \
+                -to-world ${output_dir}/${sub}/nu_in_desc-preproc_T1w_0GenericAffine.affine
 
     # Convert pial surface file to a gifti
     mris_convert ${output_dir}/${sub}/lh.pial ${output_dir}/${sub}/lh.pial.gii
 
-    # Apply the affine transformation to the pial surface file
-    wb_command -surface-apply-affine ${output_dir}/${sub}/lh.pial.gii ${output_dir}/${sub}/nu_in_desc-preproc_T1w.affine ${output_dir}/${sub}/lh.pial.native.surf.gii
+    # Apply the affine transformation matrix to the pial surface file
+    wb_command -surface-apply-affine ${output_dir}/${sub}/lh.pial.gii ${output_dir}/${sub}/nu_in_desc-preproc_T1w_0GenericAffine.affine ${output_dir}/${sub}/lh.pial.native.surf.gii
 
 done
