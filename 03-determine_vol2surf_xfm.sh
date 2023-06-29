68803bdf542b203ff84b2b16ae1e2df39d470d4e
marcjaskir <marc.jaskir@pennmedicine.upenn.edu>
2023-06-29 12:06:44 -0400

    Used FLIRT + workbench tools for computing and applying affine surface transformations

--- 03-determine_vol2surf_xfm.sh
+++ 03-determine_vol2surf_xfm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Set data and output directories
+# Set input/output directories
 data_dir=/Users/mjaskir/ngg/rotations/satterthwaite/data/jaskir_tracts
 output_dir=/Users/mjaskir/ngg/rotations/satterthwaite/outputs/vol2surf_xfm
 
-# Update SUBJECTS_DIR environmental variable
+# Update Freesurfer subjects directory
 SUBJECTS_DIR=${data_dir}/freesurfer
 
 for sub_dir in ${SUBJECTS_DIR}/sub-*; do
@@ -38,50 +38,29 @@ for sub_dir in ${SUBJECTS_DIR}/sub-*; do
         cp ${sub_dir}/mri/nu.mgz ${output_dir}/${sub}
     fi
 
-    # Convert QSIPrep preprocessed T1w image from nii.gz to mgz format
-    mri_convert --in_type nii \
-                --out_type mgz \
+    # Convert Freesurfer-derived nu.mgz image from mgz to nii.gz format
+    mri_convert --in_type mgz \
+                --out_type nii \
+                ${output_dir}/${sub}/nu.mgz \
+                ${output_dir}/${sub}/nu.nii.gz
+
+    # Compute transformation from Freesurfer-derived nu.mgz image to QSIPrep preprocessed T1w image using flirt
+    flirt -in ${output_dir}/${sub}/nu.nii.gz \
+          -ref ${output_dir}/${sub}/${sub}_desc-preproc_T1w.nii.gz \
+          -out ${output_dir}/${sub}/nu_in_desc-preproc_T1w.nii.gz \
+          -omat ${output_dir}/${sub}/nu_in_desc-preproc_T1w.mat
+    
+    # Convert FSL to nifti 'world' affine (flirt)
+    wb_command -convert-affine \
+                -from-flirt ${output_dir}/${sub}/nu_in_desc-preproc_T1w.mat \
+                ${output_dir}/${sub}/nu.nii.gz \
                 ${output_dir}/${sub}/${sub}_desc-preproc_T1w.nii.gz \
-                ${output_dir}/${sub}/${sub}_desc-preproc_T1w.mgz
+                -to-world ${output_dir}/${sub}/nu_in_desc-preproc_T1w.affine
 
-    # Compute transformation from Freesurfer-derived nu.mgz image to QSIPrep preprocessed T1w image
-    # tkregister2 --mov ${output_dir}/${sub}/nu.mgz \
-    #            --targ ${output_dir}/${sub}/${sub}_desc-preproc_T1w.mgz \
-    #            --reg ${output_dir}/${sub}/nu_in_desc-preproc_T1w.dat \
-    #            --s ${sub}
-    mri_robust_register --mov ${output_dir}/${sub}/nu.mgz \
-                        --dst ${output_dir}/${sub}/${sub}_desc-preproc_T1w.mgz \
-                        --lta ${output_dir}/${sub}/nu_in_desc-preproc_T1w.lta \
-                        --mapmov ${output_dir}/${sub}/nu_in_desc-preproc_T1w.mgz \
-                        --satit \
-                        --maxit 20 \
-                        --iscale
+    # Convert pial surface file to a gifti
+    mris_convert ${output_dir}/${sub}/lh.pial ${output_dir}/${sub}/lh.pial.gii
 
-    lta_convert --inlta ${output_dir}/${sub}/nu_in_desc-preproc_T1w.lta \
-                --outreg ${output_dir}/${sub}/nu_in_desc-preproc_T1w.dat
-
-    # As a sanity check, transform nu.mgz to QSIPrep preprocessed T1w image
-    mri_vol2vol --mov ${output_dir}/${sub}/nu.mgz \
-                --targ ${output_dir}/${sub}/${sub}_desc-preproc_T1w.mgz \
-                --reg ${output_dir}/${sub}/nu_in_desc-preproc_T1w.dat \
-                --o ${output_dir}/${sub}/nu_in_desc-preproc_T1w.mgz
-                
-
-    # Transform the subject's pial surface to be aligned with the QSIPrep preprocessed T1w image
-    mri_surf2surf --hemi lh \
-                --sval-xyz pial \
-                --tval-xyz ${output_dir}/${sub}/${sub}_desc-preproc_T1w.mgz \
-                --tval ${output_dir}/${sub}/lh_pial_in_desc_preproc_T1w \
-                --reg ${output_dir}/${sub}/nu_in_desc-preproc_T1w.dat ${output_dir}/${sub}/${sub}_desc-preproc_T1w.mgz \
-                --s ${sub}
-
-    # Map the bundle volumes to Freesurfer surfaces in native space by appling transformation
-    # test_bundle_name=ArcuateFasciculusL
-    # test_bundle_file=${data_dir}/qsirecon/${sub}/ses-V1/dwi/sub-0857566_ses-V1_space-T1w_desc-preproc_bundle-${test_bundle_name}_AutoTrackGQI.mgz
-    # mri_vol2surf --src ${test_bundle_file} \
-    #             --reg ${output_dir}/${sub}/desc-preproc_T1w-in-nu.dat \
-    #             --regheader ${sub} \
-    #             --hemi lh \
-    #             --o ${output_dir}/${sub}/${test_bundle_name}_surf.mgz
+    # Apply the affine transformation to the pial surface file
+    wb_command -surface-apply-affine ${output_dir}/${sub}/lh.pial.gii ${output_dir}/${sub}/nu_in_desc-preproc_T1w.affine ${output_dir}/${sub}/lh.pial.native.surf.gii
 
 done
