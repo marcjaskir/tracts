fa0f787fb5a7f4cfcf8d4dadb40c64dfe7da6957
marcjaskir <marc.jaskir@pennmedicine.upenn.edu>
2023-07-19 13:27:53 -0400

    Integrated normalization to fsLR 32k and 164k; Updated directory structure and comments

--- 04-map_tract_volumes_to_surfaces.sh
+++ 04-map_tract_volumes_to_surfaces.sh
@@ -11,47 +11,181 @@ for sub_dir in ${outputs_root}/sub-*; do
     # Extract subject label
     sub=$(basename ${sub_dir})
 
+    ########################################
+    # Create output directories
+    ########################################
+
     # Create output directory for surface mappings
     if [ ! -d ${outputs_root}/${sub}/surface_mappings ]; then
         mkdir -p ${outputs_root}/${sub}/surface_mappings/native
+        mkdir -p ${outputs_root}/${sub}/surface_mappings/fslr_32k
+        mkdir -p ${outputs_root}/${sub}/surface_mappings/fslr_164k
     fi
-    outputs_dir=${outputs_root}/${sub}/surface_mappings/native
+    outputs_dir_native=${outputs_root}/${sub}/surface_mappings/native
+    outputs_dir_fslr_32k=${outputs_root}/${sub}/surface_mappings/fslr_32k
+    outputs_dir_fslr_164k=${outputs_root}/${sub}/surface_mappings/fslr_164k
 
     ########################################
-    # Map tract volumes to surfaces
+    # Create midthickness surfaces from Freesurfer data (used for resampling to fsLR)
     ########################################
 
-    for tract_file in ${outputs_root}/${sub}/tracts/nifti/*_LAS.nii.gz; do
+    ###############
+    # fsLR 32k
+    ###############
+
+    # Left hemisphere
+    wb_shortcuts -freesurfer-resample-prep \
+        ${outputs_root}/${sub}/surfaces/qsiprep/lh.white.qsiprep.surf.gii \
+        ${outputs_root}/${sub}/surfaces/qsiprep/lh.pial.qsiprep.surf.gii \
+        ${outputs_root}/${sub}/surfaces/freesurfer/lh.sphere.freesurfer.surf.gii \
+        ${data_root}/templates/fslr_32k/fs_LR-deformed_to-fsaverage.L.sphere.32k_fs_LR.surf.gii \
+        ${outputs_dir_fslr_32k}/lh.midthickness.surf.gii \
+        ${outputs_dir_fslr_32k}/lh.midthickness.fslr_32k.surf.gii \
+        ${outputs_dir_fslr_32k}/lh.sphere.reg.surf.gii
+
+    # Right hemisphere
+    wb_shortcuts -freesurfer-resample-prep \
+        ${outputs_root}/${sub}/surfaces/qsiprep/rh.white.qsiprep.surf.gii \
+        ${outputs_root}/${sub}/surfaces/qsiprep/rh.pial.qsiprep.surf.gii \
+        ${outputs_root}/${sub}/surfaces/freesurfer/rh.sphere.freesurfer.surf.gii \
+        ${data_root}/templates/fslr_32k/fs_LR-deformed_to-fsaverage.R.sphere.32k_fs_LR.surf.gii \
+        ${outputs_dir_fslr_32k}/rh.midthickness.surf.gii \
+        ${outputs_dir_fslr_32k}/rh.midthickness.fslr_32k.surf.gii \
+        ${outputs_dir_fslr_32k}/rh.sphere.reg.surf.gii
+
+    ###############
+    # fsLR 164k
+    ###############
+
+    # Left hemisphere
+    wb_shortcuts -freesurfer-resample-prep \
+        ${outputs_root}/${sub}/surfaces/qsiprep/lh.white.qsiprep.surf.gii \
+        ${outputs_root}/${sub}/surfaces/qsiprep/lh.pial.qsiprep.surf.gii \
+        ${outputs_root}/${sub}/surfaces/freesurfer/lh.sphere.freesurfer.surf.gii \
+        ${data_root}/templates/fslr_164k/fs_LR-deformed_to-fsaverage.L.sphere.164k_fs_LR.surf.gii \
+        ${outputs_dir_fslr_164k}/lh.midthickness.surf.gii \
+        ${outputs_dir_fslr_164k}/lh.midthickness.fslr_164k.surf.gii \
+        ${outputs_dir_fslr_164k}/lh.sphere.reg.surf.gii
+
+    # Right hemisphere
+    wb_shortcuts -freesurfer-resample-prep \
+        ${outputs_root}/${sub}/surfaces/qsiprep/rh.white.qsiprep.surf.gii \
+        ${outputs_root}/${sub}/surfaces/qsiprep/rh.pial.qsiprep.surf.gii \
+        ${outputs_root}/${sub}/surfaces/freesurfer/rh.sphere.freesurfer.surf.gii \
+        ${data_root}/templates/fslr_164k/fs_LR-deformed_to-fsaverage.R.sphere.164k_fs_LR.surf.gii \
+        ${outputs_dir_fslr_164k}/rh.midthickness.surf.gii \
+        ${outputs_dir_fslr_164k}/rh.midthickness.fslr_164k.surf.gii \
+        ${outputs_dir_fslr_164k}/rh.sphere.reg.surf.gii
+
+    ########################################
+    # Map tract volumes to native surfaces
+    ########################################
+
+    for tract_file in ${outputs_root}/${sub}/tracts/nifti/native_orientation-LAS/*; do
 
         # Extract tract label
         tract_fname=$(basename ${tract_file})
         tract_label=$(echo ${tract_fname} | sed 's/_LAS.nii.gz//g')
-
-        # Extract last character of tract label
-        hemi=$(echo ${tract_label} | rev | cut -c1 | rev)
-
-        # Check if its a commisural tract
-        if [ "${hemi}" != "L" ] && [ "${hemi}" != "R" ]; then
             
-            # Map tract to surfaces
-            wb_command -volume-to-surface-mapping ${tract_file} ${outputs_root}/${sub}/surfaces/lh.pial.qsiprep.surf.gii ${outputs_dir}/${tract_label}.lh.shape.gii -trilinear
-            wb_command -volume-to-surface-mapping ${tract_file} ${outputs_root}/${sub}/surfaces/rh.pial.qsiprep.surf.gii ${outputs_dir}/${tract_label}.rh.shape.gii -trilinear
+        # Map tract to native surfaces
+        wb_command -volume-to-surface-mapping \
+            ${tract_file} \
+            ${outputs_root}/${sub}/surfaces/qsiprep/lh.pial.qsiprep.surf.gii \
+            ${outputs_dir_native}/${tract_label}.lh.shape.gii \
+            -trilinear
+        wb_command -volume-to-surface-mapping \
+            ${tract_file} \
+            ${outputs_root}/${sub}/surfaces/qsiprep/rh.pial.qsiprep.surf.gii \
+            ${outputs_dir_native}/${tract_label}.rh.shape.gii \
+            -trilinear
 
-        else
+    done
 
-            # Map tract to the corresponding hemisphere's surface
-            if [ "${hemi}" == "L" ]; then
-                
-                wb_command -volume-to-surface-mapping ${tract_file} ${outputs_root}/${sub}/surfaces/lh.pial.qsiprep.surf.gii ${outputs_dir}/${tract_label}.lh.shape.gii -trilinear
+    ########################################
+    # Warp tract surfaces to fsLR
+    ########################################
 
-            elif [ "${hemi}" == "R" ]; then
+    for tract_file in ${outputs_dir_native}/*.shape.gii; do
 
-                wb_command -volume-to-surface-mapping ${tract_file} ${outputs_root}/${sub}/surfaces/rh.pial.qsiprep.surf.gii ${outputs_dir}/${tract_label}.rh.shape.gii -trilinear
+        # Extract tract label
+        tract_fname=$(basename ${tract_file})
+        tract_label=$(echo ${tract_fname} | sed 's/.shape.gii//g')
 
-            fi
+        echo "Warping ${tract_label} to fsLR"
+
+        # Extract last 2 characters of tract label
+        hemi=$(echo ${tract_label} | rev | cut -c1-2 | rev)
+
+        ###############
+        # fsLR 32k
+        ###############
+
+         # Left hemisphere
+        if [ ${hemi} == 'lh' ]; then
+
+            wb_command -metric-resample \
+                ${tract_file} \
+                ${outputs_root}/${sub}/surfaces/freesurfer/lh.sphere.freesurfer.surf.gii \
+                ${data_root}/templates/fslr_32k/fs_LR-deformed_to-fsaverage.L.sphere.32k_fs_LR.surf.gii \
+                ADAP_BARY_AREA \
+                ${outputs_dir_fslr_32k}/${tract_label}.shape.gii \
+                -area-surfs ${outputs_dir_fslr_32k}/lh.midthickness.surf.gii ${outputs_dir_fslr_32k}/lh.midthickness.fslr_32k.surf.gii
+
+        # Right hemisphere
+        elif [ ${hemi} == 'rh' ]; then
+
+            wb_command -metric-resample \
+                ${tract_file} \
+                ${outputs_root}/${sub}/surfaces/freesurfer/rh.sphere.freesurfer.surf.gii \
+                ${data_root}/templates/fslr_32k/fs_LR-deformed_to-fsaverage.R.sphere.32k_fs_LR.surf.gii \
+                ADAP_BARY_AREA \
+                ${outputs_dir_fslr_32k}/${tract_label}.shape.gii \
+                -area-surfs ${outputs_dir_fslr_32k}/rh.midthickness.surf.gii ${outputs_dir_fslr_32k}/rh.midthickness.fslr_32k.surf.gii
+
+        fi       
+
+        ###############
+        # fsLR 164k
+        ###############
+
+        # Left hemisphere
+        if [ ${hemi} == 'lh' ]; then
+
+            wb_command -metric-resample \
+                ${tract_file} \
+                ${outputs_root}/${sub}/surfaces/freesurfer/lh.sphere.freesurfer.surf.gii \
+                ${data_root}/templates/fslr_164k/fs_LR-deformed_to-fsaverage.L.sphere.164k_fs_LR.surf.gii \
+                ADAP_BARY_AREA \
+                ${outputs_dir_fslr_164k}/${tract_label}.shape.gii \
+                -area-surfs ${outputs_dir_fslr_164k}/lh.midthickness.surf.gii ${outputs_dir_fslr_164k}/lh.midthickness.fslr_164k.surf.gii
+
+        # Right hemisphere
+        elif [ ${hemi} == 'rh' ]; then
+
+            wb_command -metric-resample \
+                ${tract_file} \
+                ${outputs_root}/${sub}/surfaces/freesurfer/rh.sphere.freesurfer.surf.gii \
+                ${data_root}/templates/fslr_164k/fs_LR-deformed_to-fsaverage.R.sphere.164k_fs_LR.surf.gii \
+                ADAP_BARY_AREA \
+                ${outputs_dir_fslr_164k}/${tract_label}.shape.gii \
+                -area-surfs ${outputs_dir_fslr_164k}/rh.midthickness.surf.gii ${outputs_dir_fslr_164k}/rh.midthickness.fslr_164k.surf.gii
 
         fi
 
     done
+
+    # Clean up intermediary files
+    rm ${outputs_dir_fslr_32k}/lh.midthickness.surf.gii
+    rm ${outputs_dir_fslr_32k}/rh.midthickness.surf.gii
+    rm ${outputs_dir_fslr_32k}/lh.sphere.reg.surf.gii
+    rm ${outputs_dir_fslr_32k}/rh.sphere.reg.surf.gii
+    rm ${outputs_dir_fslr_32k}/lh.midthickness.fslr_32k.surf.gii
+    rm ${outputs_dir_fslr_32k}/rh.midthickness.fslr_32k.surf.gii
+    rm ${outputs_dir_fslr_164k}/lh.midthickness.surf.gii
+    rm ${outputs_dir_fslr_164k}/rh.midthickness.surf.gii
+    rm ${outputs_dir_fslr_164k}/lh.sphere.reg.surf.gii
+    rm ${outputs_dir_fslr_164k}/rh.sphere.reg.surf.gii
+    rm ${outputs_dir_fslr_164k}/lh.midthickness.fslr_164k.surf.gii
+    rm ${outputs_dir_fslr_164k}/rh.midthickness.fslr_164k.surf.gii
 
 done
